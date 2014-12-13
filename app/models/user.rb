7153c36631864a0066f821d53b91967eb7f109b2
poopoothegorilla <jm.wlkr1@gmail.com>
2014-12-13 13:25:08 -0500

    add associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  has_many :userviews
+  validates :name, :password, :session_token, presence: true
+
+  has_secure_password
+
+  has_many :user_views
+  has_many :user_saves
+
+  has_many :saved_tips, through: :user_saves, source: :tip_id
+  has_many :viewed_tips, through: :user_views, source: :tip_id
 end
--- db/migrate/20141213171816_create_users.rb
+++ db/migrate/20141213171816_create_users.rb
@@ -2,9 +2,12 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
 
       t.timestamps
     end
+    add_index :users, :session_token
     add_index :users, :name
   end
 end
