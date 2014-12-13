ca638d6fb91057f29e75cb33bcc0644e60f59b09
poopoothegorilla <jm.wlkr1@gmail.com>
2014-12-13 12:42:47 -0500

    add migrations for users tips and userviews tables

--- db/migrate/20141213171857_create_tips.rb
+++ db/migrate/20141213171857_create_tips.rb
@@ -1,8 +1,18 @@
 class CreateTips < ActiveRecord::Migration
   def change
     create_table :tips do |t|
+      t.string :name
+      t.text :text, null: false
+      t.string :email
+      t.string :city
+      t.string :state
 
       t.timestamps
     end
+
+    add_index :tips, :name
+    add_index :tips, :city
+    add_index :tips, :state
+
   end
 end
--- db/migrate/20141213171931_create_user_views.rb
+++ db/migrate/20141213171931_create_user_views.rb
@@ -1,8 +1,12 @@
 class CreateUserViews < ActiveRecord::Migration
   def change
     create_table :user_views do |t|
+      t.integer :user_id
+      t.integer :tip_id
 
       t.timestamps
     end
+    add_index :user_views, :user_id
+    add_index :user_views, :tip_id
   end
 end
