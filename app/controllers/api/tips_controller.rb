a02131f7b0afa0806afa537da156c4b9fecf1428
Stefan Huynh <huynh.stefan@gmail.com>
2014-12-13 16:19:14 -0500

    Add json views and search in tips_controller

--- app/controllers/api/tips_controller.rb
+++ app/controllers/api/tips_controller.rb
@@ -2,7 +2,12 @@ class Api::TipsController < ApplicationController
   before_filter require_signed_in!
 
   def index
-    @tips = Tip.all
+    if params[:tip][:query]
+      @tips = Tip.search_by_subject_and_text(params[:tip][:query])
+    else
+      @tips = Tip.all
+    end
+
     render :index, status: :ok
   end
 
