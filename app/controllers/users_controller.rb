ded0f33818b7291266fc2fb00757d4ce36d34395
dyg2104 <dyg2104@gmail.com>
2014-12-13 15:20:13 -0500

    signs in user when they sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+      sign_in!(user)
       redirect_to dashboard_url
     else
       flash.now[:errors] = @user.errors.full_messages
