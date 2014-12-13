874fcdb2492117d425f343fea1c0df32bfa73efc
Stefan Huynh <huynh.stefan@gmail.com>
2014-12-13 14:26:05 -0500

    Redirect to new_tip_url instead of root_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
     if user.authenticate(params[:session][:password])
       sign_in!(user)
-      redirect_to root_url
+      redirect_to new_tip_url
     else
       flash.now[:errors] = ['Invalid username/password combination']
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url
+      redirect_to new_tip_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
