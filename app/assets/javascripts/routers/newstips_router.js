c1abeb878e59d107a6186245ddacbb683571adc1
poopoothegorilla <jm.wlkr1@gmail.com>
2014-12-13 18:54:14 -0500

    fix routes

--- app/assets/javascripts/routers/newstips_router.js
+++ app/assets/javascripts/routers/newstips_router.js
@@ -23,16 +23,20 @@ Newstips.Routers.NewstipsRouter = Backbone.Router.extend({
 
 	search: function () {
 		var view = new Newstips.Views.SearchShow()
+		this.root.html(view.render().$el)
 	},
 
 	searchResults: function (query) {
 		var tips = new Newstips.Collections.Tips();
-		tips.fetch({
-			success: function({
-				var view = new Newstips.Views.IndexShow({
-					// collection: Newstips.Collections.
-				});
-			})
-		})
+		$.get(
+			"/api/tips?query=" + query
+		).success(function(data){
+			console.log("SUCESS");
+			console.log(data);
+			tips.add(data);
+		});
+
+		var tipsShow = new Newstips.Views.Tips({ collection: tips });
+		this.root.html(tipsShow.render().$el);
 	}
 })
