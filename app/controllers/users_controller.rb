a5f6022b995560bc6204d260126fe21e8a0faf2a
dyg2104 <dyg2104@gmail.com>
2014-12-13 15:21:16 -0500

    fixes sign in on sign up controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      sign_in!(user)
+      sign_in!(@user)
       redirect_to dashboard_url
     else
       flash.now[:errors] = @user.errors.full_messages
