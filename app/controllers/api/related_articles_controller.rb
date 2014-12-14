b883a25eb1cf7dee06628773370f86bece30eea8
Stefan Huynh <huynh.stefan@gmail.com>
2014-12-13 19:55:02 -0500

    Only return headline

--- app/controllers/api/related_articles_controller.rb
+++ app/controllers/api/related_articles_controller.rb
@@ -7,7 +7,8 @@ class Api::RelatedArticlesController < ApplicationController
     response = Nyt.invoke(city: city, subject: subject)['response']['docs']
     @related_articles = []
     response.each do |article|
-      @related_articles << { url: article['web_url'], headline: article['headline']}
+      # @related_articles << { url: article['web_url'], headline: article['headline']}
+      @related_articles << article['headline']['main']
     end
   end
 
